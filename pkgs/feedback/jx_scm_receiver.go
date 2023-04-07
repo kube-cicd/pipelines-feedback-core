39a72c9f9b3f6bc43fedd4bf5e75315d921a74c1
Damian Kęska <372403+keskad@users.noreply.github.com>
2023-04-07 21:16:40 +0200

    feat: Initial jx go-scm implementation that works - PR comments and commit status, tested on Github

--- pkgs/feedback/jx_scm_receiver.go
+++ pkgs/feedback/jx_scm_receiver.go
@@ -2,12 +2,12 @@ package feedback
 
 import (
 	"context"
-	"fmt"
 	"github.com/Kubernetes-Native-CI-CD/pipelines-feedback-core/pkgs/contract"
 	"github.com/Kubernetes-Native-CI-CD/pipelines-feedback-core/pkgs/contract/wiring"
 	"github.com/jenkins-x/go-scm/scm"
 	"github.com/jenkins-x/go-scm/scm/factory"
 	"github.com/pkg/errors"
+	"strconv"
 )
 
 type JXSCMReceiver struct {
@@ -28,53 +28,98 @@ func (jx *JXSCMReceiver) InitializeWithContext(sc *wiring.ServiceContext) error 
 	return nil
 }
 
-func (jx *JXSCMReceiver) Update(ctx context.Context, pipeline contract.PipelineInfo) error {
+func (jx *JXSCMReceiver) WhenCreated(ctx context.Context, pipeline contract.PipelineInfo) error {
+	if pipeline.GetSCMContext().PrId == "" {
+		jx.sc.Log.Debug("Missing PR id, skipping")
+		return nil
+	}
+	prId, convErr := strconv.Atoi(pipeline.GetSCMContext().PrId)
+	if convErr != nil {
+		jx.sc.Log.Warnf("PR id has wrong format, should be an integer, got '%v'", pipeline.GetSCMContext().PrId)
+		return nil
+	}
+
+	// :white_check_mark: / :x: / :hourglass_flowing_sand:
+
+	pr, _, _ := jx.client.PullRequests.ListComments()
+
+	// Create PR comment
+	_, _, err := jx.client.PullRequests.CreateComment(ctx, pipeline.GetSCMContext().GetNameWithOrg(), prId, &scm.CommentInput{
+		Body: "The Pipeline " + pipeline.GetStatus().AsHumanReadableDescription(),
+	})
+	if err != nil {
+		return errors.Wrap(err, "cannot update Pull Request status")
+	}
+	return nil
+}
+
+func (jx *JXSCMReceiver) WhenStarted(ctx context.Context, pipeline contract.PipelineInfo) error {
+	return nil
+}
+
+func (jx *JXSCMReceiver) WhenFinished(ctx context.Context, pipeline contract.PipelineInfo) error {
+	return nil
+}
+
+func (jx *JXSCMReceiver) UpdateProgress(ctx context.Context, pipeline contract.PipelineInfo) error {
 	jx.sc.Log.Infoln("JXSCMReceiver", pipeline)
 
 	scmCtx := pipeline.GetSCMContext()
-	overallStatus := jx.translateStatus(pipeline.GetStatus())
+	ourStatus := pipeline.GetStatus()
+	overallStatus := jx.translateStatus(ourStatus)
+
+	var commitStatusErr error = nil
+	var prStatusErr error = nil
 
 	if jx.client == nil {
 		return errors.New("jx.client is nil")
 	}
 
-	if jx.client.Commits == nil {
-		return errors.New("jx.client.Commits is nil")
+	if jx.client.Repositories != nil {
+		// todo: Create status for multiple stages?
+		// todo: Choose between agregated status vs per-stage status?
+
+		_, _, commitStatusErr = jx.client.Repositories.CreateStatus(ctx, pipeline.GetSCMContext().GetNameWithOrg(),
+			scmCtx.Commit, &scm.StatusInput{
+				State:  overallStatus,
+				Label:  "pipelines-feedback",
+				Desc:   ourStatus.AsHumanReadableDescription(),
+				Target: "", // todo: URL
+			},
+		)
+	} else {
+		jx.sc.Log.Warning("jx.client.Repositories is nil. No support for commit status update for this SCM provider in jx go-scm?")
 	}
 
-	_, response, err := jx.client.Commits.UpdateCommitStatus(ctx, scmCtx.GetNameWithOrg(), scmCtx.Commit, &scm.CommitStatusUpdateOptions{
-		ID:          scmCtx.PrId,
-		Sha:         pipeline.GetSCMContext().Commit,
-		State:       overallStatus,
-		Ref:         scmCtx.Reference,
-		Name:        pipeline.GetFullName(),
-		TargetURL:   pipeline.GetUrl(),
-		Description: "",
-	})
-
-	if err != nil {
-		return errors.Wrap(err, "cannot update commit status")
-	}
-	if response.Status > 299 {
-		return errors.New(fmt.Sprintf("cannot update commit status: got HTTP %v error", response.Status))
+	// Pull/Merge Requests
+	if jx.client.PullRequests != nil {
+		// todo: Update PR status
+	} else {
+		jx.sc.Log.Warning("jx.client.PullRequests is nil. No support for manipulating Pull/Merge Requests for this SCM provider in jx go-scm? Cannot operate on Pull/Merge Requests")
 	}
 
+	if commitStatusErr != nil {
+		return commitStatusErr
+	}
+	if prStatusErr != nil {
+		return prStatusErr
+	}
 	return nil
 }
 
-func (jx *JXSCMReceiver) translateStatus(status contract.Status) string {
+func (jx *JXSCMReceiver) translateStatus(status contract.Status) scm.State {
 	switch status {
 	case contract.Running:
-		return "running"
+		return scm.StateRunning
 	case contract.Pending:
-		return "pending"
+		return scm.StatePending
 	case contract.Succeeded:
-		return "success"
+		return scm.StateSuccess
 	case contract.Errored:
-		return "error"
+		return scm.StateError
 	case contract.Failed:
-		return "failed"
+		return scm.StateFailure
 	default:
-		return "pending"
+		return scm.StateUnknown
 	}
 }
