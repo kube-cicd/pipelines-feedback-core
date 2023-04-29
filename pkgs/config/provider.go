457574e0c6577b36d97846287cce1c3f01a4980f
Damian Kęska <372403+keskad@users.noreply.github.com>
2023-04-29 20:43:24 +0200

    feat: Add Get() method for shorter end-implementation code

--- pkgs/config/provider.go
+++ pkgs/config/provider.go
@@ -21,6 +21,12 @@ func (d Data) HasKey(keyName string) bool {
 	return hasKey
 }
 
+// Get retrieves a configuration value. For non-existing key it returns an empty string
+func (d Data) Get(keyName string) string {
+	return d.GetOrDefault(keyName, "")
+}
+
+// GetOrDefault retrieves a configuration value. For non-existing keys it returns a default value defined in `defaultVal` parameter
 func (d Data) GetOrDefault(keyName string, defaultVal string) string {
 	val, hasKey := d[keyName]
 	if hasKey {
