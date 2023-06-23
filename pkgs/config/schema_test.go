2cf234afa3a3ededf9e71ff3fe6efd3f55b4f17a
Damian Kęska <372403+keskad@users.noreply.github.com>
2023-06-23 21:27:42 +0200

    tests: Improve coverage

---
+++ pkgs/config/schema_test.go
@@ -0,0 +1,43 @@
+package config_test
+
+import (
+	"github.com/Kubernetes-Native-CI-CD/pipelines-feedback-core/pkgs/apis/pipelinesfeedback.keskad.pl/v1alpha1"
+	"github.com/Kubernetes-Native-CI-CD/pipelines-feedback-core/pkgs/config"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestSchemaValidator_ValidateRequestedEntry(t *testing.T) {
+	validator := config.SchemaValidator{}
+	validator.Add(config.Schema{
+		Name:          "kropotkin",
+		AllowedFields: []string{"bread", "book"},
+	})
+	assert.Nil(t, validator.ValidateRequestedEntry("kropotkin", "bread"))
+	assert.NotNil(t, validator.ValidateRequestedEntry("kropotkin", "this-does-not-exist"))
+}
+
+func TestSchemaValidator_ValidateConfig_Scoped(t *testing.T) {
+	validator := config.SchemaValidator{}
+	validator.Add(config.Schema{
+		Name:          "kropotkin",
+		AllowedFields: []string{"bread", "book"},
+	})
+	assert.Nil(t, validator.ValidateConfig(v1alpha1.Data{
+		"kropotkin.bread": "The Conquest of bread",
+	}))
+	assert.Contains(t, validator.ValidateConfig(v1alpha1.Data{
+		"kropotkin.blabla": "This key does not exist",
+	}).Error(), "field 'blabla' is not a valid field for component 'kropotkin'")
+}
+
+func TestSchemaValidator_ValidateConfig_Global(t *testing.T) {
+	validator := config.SchemaValidator{}
+	validator.Add(config.Schema{
+		Name:          "global",
+		AllowedFields: []string{"bread", "book"},
+	})
+	assert.Nil(t, validator.ValidateConfig(v1alpha1.Data{
+		"bread": "The Conquest of bread",
+	}))
+}
